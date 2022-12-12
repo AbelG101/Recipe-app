d04439456740b5d61a80d15fe73a43840a1a276e
AbelG101 <abelgebeyehu101@gmail.com>
2022-12-12 16:55:55 +0300

    Add on delete cascade for recipes migration

---
+++ db/migrate/20221212135119_update_foreign_key_add_on_delete_constraint.rb
@@ -0,0 +1,7 @@
+class UpdateForeignKeyAddOnDeleteConstraint < ActiveRecord::Migration[7.0]
+  def change
+    remove_foreign_key "recipes", "users"
+
+    add_foreign_key "recipes", "users", on_delete: :cascade
+  end
+end
