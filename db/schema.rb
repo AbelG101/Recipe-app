8f21c6dfbf7fc8f2c98c1d79f77e29b923a596e9
AbelG101 <abelgebeyehu101@gmail.com>
2022-12-12 16:55:33 +0300

    Update table - add on delete cascade

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_134345) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_135119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,5 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_134345) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "recipes", "users"
+  add_foreign_key "recipes", "users", on_delete: :cascade
 end
