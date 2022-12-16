29b8c728235eaf09da0bbbfcee5d6f758d3f514e
Betsegaw Sebsibe <betsesebe@gmail.com>
2022-12-16 14:13:49 +0300

    Fix confirmable issue

--- db/migrate/20221212133216_create_users.rb
+++ db/migrate/20221212133216_create_users.rb
@@ -2,12 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
+
       t.timestamps
     end
-    add_index  :users, :confirmation_token, :unique => true
   end
 end
